33393efbdd61d5b4d860f7b093207d8eb6ef91fb
Vafin Artem <a.vafin85@gmail.com>
2020-09-02 18:29:16 +0500

    Сделал открытие и закрытие popup через js

--- source/js/script.js
+++ source/js/script.js
@@ -20,6 +20,35 @@ menuButton.addEventListener("click", function (evt) {
 }
 )
 
+let addCart = document.querySelectorAll(".js-add-cart");
+let cartPopup = document.querySelector(".cart");
+let popup = cartPopup.querySelector(".cart__wrapper");
+
+for (let i = 0; i < addCart.length; i++) {
+  addCart[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.add("cart--active");
+  })
+};
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (cartPopup.classList.contains("cart--active")) {
+      evt.preventDefault();
+      cartPopup.classList.remove("cart--active");
+    }
+  }
+});
+
+cartPopup.addEventListener("mousedown", function (evt) {
+  cartPopup.classList.remove("cart--active");
+});
+
+popup.addEventListener("mousedown", function (e) {
+  e.stopPropagation();
+});
+
+
 function initMap() {
   let academy = { lat: 59.938635, lng: 30.323118 };
   let map = new google.maps.Map(
